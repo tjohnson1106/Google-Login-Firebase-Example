6a03ce7cf7917d165fa7e453922ad51f1e045f7a
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2019-01-16 19:48:54 -0500

    created App Switch Navigator configuration

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createSwitchNavigator } from "react-navigation";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
